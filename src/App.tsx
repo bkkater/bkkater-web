1b432083573c68a8881313d5e6105469fa1ab87d
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-04 02:02:48 -0300

    feat: Animações como tema do tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   console.log(theme);
 
   return (
-    <div className='dark:bg-zinc-900 bg-zinc-300 text-zinc-600 dark:text-zinc-300 flex flex-col h-screen scroll-smooth'>
+    <div className='dark:bg-zinc-900 bg-zinc-300 text-zinc-600 dark:text-zinc-300 flex flex-col h-screen scroll-smooth animate-fadeIn'>
       <Header />
 
       <Landing />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fromTop: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-30px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        fromLeft: {
+          from: {
+            opacity: '0',
+            transform: 'translateX(-30px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        fadeIn: {
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        fromTop: 'fromTop 0.5s ease-out',
+        fromLeft: 'fromLeft 0.5s ease-out',
+        fadeIn: 'fadeIn 0.5s ease-out',
+      },
+    },
   },
   plugins: [],
 };
